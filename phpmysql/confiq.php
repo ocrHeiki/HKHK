5fd7ed6ead8f333a582a3ba3292e706981c8207d
Heiki Rebane <156500254+ocrHeiki@users.noreply.github.com>
2024-04-04 10:12:14 +0300

    up

--- phpmysql/confiq.php
+++ phpmysql/confiq.php
@@ -1,14 +1,13 @@
 <?php
-$kasutaja ="heiki";
-$dbserver = "localhost";
-$andmebaas = "muusikapood";
-$pw = "J00k5m1n3";
+    $kasutaja = "mario";
+    $dbserver = "localhost";
+    $andmebaas = "muusikapood";
+    $pw = "mario";
 
-$yhendus = mysqli_connect($dbserver, $kasutaja, $pw, $andmebaas);
+    $yhendus = mysqli_connect($dbserver, $kasutaja, $pw, $andmebaas);
 
-if (!$yhendus){ //"!" hüümärk muudab IF funktsiooni eitavaks
-    echo "Jama majas";
-    die("Sa jälle ebaõnnestusid!");
-}
+    if(!$yhendus){
+        die("Sa jälle ebaõnnestusid!");
+    } 
 
 ?>
